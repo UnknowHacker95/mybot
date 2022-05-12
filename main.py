17bfc182d847c411ce91ca207ef2a5126e6f4dcf
UnknowHacker95 <unhack9582@gmail.com>
2022-05-12 13:18:39 +0700

    Finally

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import telebot
 from nlgu import NLU
+from nlgu import text
+from nlgu import illustrachiya
 API_TOKEN = ''  # TODO: Вставьте сюда API_TOKEN вашего бота
 answer =[]
 bot = telebot.TeleBot(API_TOKEN)
 controller = {}
+il = {}
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -14,6 +17,7 @@ def start_message(message):
 Для начала напишите “Хочу историю”''')
     id = message.from_user.id
     controller[id] = 'start'
+    il[id] = 0
 
 @bot.message_handler(content_types=['text'])
 def start(message):
@@ -21,5 +25,64 @@ def start(message):
     user_choice = message.text
     user_state = controller.get(user_id, 'start')
     first = NLU()
-    first.right(user_choice, user_state)
+    if(user_state == 'start'):
+        if(first.right(user_choice,user_state) != 1):
+            bot.send_message(message.chat.id, "Напишите 'Хочу историю' ")
+        else:
+            controller[user_id] = 'vibor'
+            bot.send_message(message.chat.id, "Вы хотите иллюстрацию к своей истории?")
+    if(user_state == 'start_t'):
+        if(user_choice == 'Да'):
+            controller[user_id] = 'start'
+        else:
+            bot.send_message(message.chat.id, "Ну пока! Если что, то напишите 'Хочу историю'")
+            controller[user_id] = 'start'
+    if (user_state == 'vibor'):
+        if (first.right(user_choice, user_state) == 'Error'):
+            bot.send_message(message.chat.id, "Ответьте нормально на вопрос")
+        elif (first.right(user_choice, user_state) == 1):
+            il[user_id] = 1
+            controller[user_id] = 'tema'
+            bot.send_message(message.chat.id, "Введите темы")
+        else:
+            controller[user_id] = 'tema'
+            bot.send_message(message.chat.id, "Введите темы")
+    if(user_state == 'tema'):
+        textix = first.right(user_choice, user_state)
+        history = text(textix)
+        bot.send_message(message.chat.id, history)
+        if (il[user_id] == 1):
+            bot.send_photo(message.chat.id, illustrachiya(textix[0]))
+        bot.send_message(message.chat.id, "Хотите еще историю?")
+        controller[user_id] = 'start_t'
+        il[user_id] = 0
+    else:
+        pass
+@bot.message_handler(content_types=['sticker'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+'''Красиво конечно, но давайте по делу''')
+
+@bot.message_handler(content_types=['video'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+'''Красиво конечно, но давайте по делу''')
+
+@bot.message_handler(content_types=['photo'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+'''.....''')
+    bot.send_message(message.chat.id, "Вы меня не любите(((ТТ(((")
+bot.polling()
+
+@bot.message_handler(content_types=['document'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+'''Не игнорирую МЕНЯ!!!!''')
+
+@bot.message_handler(content_types=['audio'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+'''Не игнорирую МЕНЯ!!!!''')
+
 bot.polling()
--- nlgu.py
+++ nlgu.py
@@ -1,5 +1,5 @@
-import googletrans
 from googletrans import Translator
+
 translator = Translator()
 
 def number(text):
@@ -9,7 +9,7 @@ class NLU:
     def __init__(self):
         pass
     def right(self, message, state):
-        if state == 'tema':
+        if state == 'vibor':
             if (message =='Да'):
                 return 1
             elif (message == 'Нет'):
@@ -21,6 +21,11 @@ class NLU:
                 return 1
             else:
                 return 'Error'
+        elif state == 'start':
+            if(message == 'Хочу историю'):
+                return 1
+            else:
+                return 'Error'
         else:
             if(number(message) == 'True'):
                 return 'Error'
@@ -28,7 +33,32 @@ class NLU:
                 if(len(message) < 3):
                     return 'Error'
                 tems = message.split()
-                tems[0] = translator.translate('папа')
-                tems[1] = translator.translate('мир')
+                tems[0] = translator.translate(tems[0], src='ru', dest='en').text
+                tems[1] = translator.translate(tems[1], src='ru', dest='en').text
                 return tems
 
+def text(a):
+    import requests
+    s = a[0] + ' and ' + a[1]
+    r = requests.post(
+        "https://api.deepai.org/api/text-generator",
+        data={
+            'text': s,
+        },
+        headers={'api-key': '89116dba-6d4a-4c86-977a-04b7d8a82029'}
+    )
+    string = r.json()['output']
+    string = translator.translate(string, src='en', dest='ru').text
+    return string
+
+def illustrachiya(b):
+    import requests
+    s = translator.translate(b, src='ru', dest='en').text
+    r = requests.post(
+        "https://api.deepai.org/api/text2img",
+        data={
+            'text': s,
+        },
+        headers={'api-key': '89116dba-6d4a-4c86-977a-04b7d8a82029'}
+    )
+    return r.json()['output_url']
